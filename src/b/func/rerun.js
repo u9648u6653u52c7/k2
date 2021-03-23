232c4eb3e892d140a1ea81ca12e4b62c874c2e9c
zer0 <u9648u6653u52c7@gmail.com>
2021-03-23 11:38:01 +0800

    feat(b/func/rerun):调整依赖包导入、优化调用方式

--- src/b/func/rerun.js
+++ src/b/func/rerun.js
@@ -1,22 +1,5 @@
-const noop = () => { };
-const slice = [].slice;
-
-/** PRD 生成个具有重试机制的方法
- * 重试策略最终由使用者决定；
- * 包装生成的方法必须能正常转发业务方法所需的参数，并给业务方法提供重新调用自己的能力；
- * 为重试提供次数限制，经过重试后，若最终结果是消极的，由用户进行相应的兜底处理；
- * 注意内存管理问题；
- *
- * const args = [].slice.call(arguments);
- * 需考虑多次调用的副作用，决策应对的方式；
- * const getUserData = rerun(()=>{}, ()=>{});
- * getUserData(1);
- * getUserData(2);
- * getUserData(3);
- * 只响应第一次调用、或者最后一次调用？还是怎么着呢？
- * 加入延迟执行的功能，防止触发防频风控。
- */
-
+import slice from "../var/slice";
+import isFunction from "../type/isFunction";
 
 /**
  * rerun
@@ -26,8 +9,12 @@ const slice = [].slice;
  * @returns {Function}
  */
 function rerun(func, polyfill, options) {
-    if (typeof func !== "function") {
-        return noop;
+    if (!isFunction(func)) {
+        return () => { };
+    }
+
+    if (!isFunction(polyfill)) {
+        options = polyfill;
     }
 
     const cfg = Object.assign({
@@ -45,11 +32,11 @@ function rerun(func, polyfill, options) {
 
         function run() {
             if (overflow) {
-                if (typeof polyfill === "function") {
+                if (isFunction(polyfill)) {
                     const _args = slice.call(arguments);
                     polyfill.apply(ctx, _args);
                 }
-                func = polyfill = ctx = args = null;
+                ctx = args = run = null;
             } else {
                 overflow = ++acc > cfg.n ? true : false;
                 if (acc > 1 && cfg.async) {
