f27a82cda45029acc3af7287d4f1038781cf7810
zer0 <u9648u6653u52c7@gmail.com>
2021-04-02 23:17:55 +0800

    feat(b/type/isEmpty):enhance

--- src/b/type/isEmptyObject.js
+++ src/b/type/isEmptyObject.js
@@ -1,5 +1,22 @@
-export default function (val) {
-    var name;
-    for (name in val) { return false; }
+import isString from "./isString";
+import isArray from "./isArray";
+import isObject from "./isObject";
+import hasOwn from "../var/hasOwn";
+
+export default function (value) {
+    if (value == null) { return true; }
+
+    if (isString(value) || isArray(value)) {
+        return value.length == 0;
+    }
+
+    if (isObject(value)) {
+        for (const key in value) {
+            if (hasOwn.call(value, key)) {
+                return false;
+            }
+        }
+    }
+
     return true;
 }
